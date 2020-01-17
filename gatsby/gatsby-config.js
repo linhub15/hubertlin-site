f86ab3e3c35f8f6aee897598b631bcadfbbd46ff
Hubert Lin - Macbook <linhub15@gmail.com>
2020-01-16 23:49:43 -0700

    convert gatsby-config to es6 module

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,35 +1,33 @@
-module.exports = {
-  siteMetadata: {
-    title: `My developer journey`,
-    description: `TODO`,
-    author: `Hubert Lin`
-  },
-  plugins: [
-    `gatsby-plugin-catch-links`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    },
-    {
-      resolve: `gatsby-transformer-remark`
-    }
-  ]
+export const siteMetadata = {
+  title: `My developer journey`,
+  description: `TODO`,
+  author: `Hubert Lin`
 };
+export const plugins = [
+  `gatsby-plugin-catch-links`,
+  `gatsby-plugin-react-helmet`,
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`
+    }
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `gatsby-starter-default`,
+      short_name: `starter`,
+      start_url: `/`,
+      background_color: `#663399`,
+      theme_color: `#663399`,
+      display: `minimal-ui`,
+      icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+    }
+  },
+  {
+    resolve: `gatsby-transformer-remark`
+  }
+];
