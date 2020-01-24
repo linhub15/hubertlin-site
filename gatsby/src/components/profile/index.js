27e2d4fe36b2373cdd8dd8df805fc086bc02d3e1
Hubert Lin - Macbook <linhub15@gmail.com>
2020-01-23 22:07:35 -0700

    center the profile component

--- gatsby/src/components/profile/index.js
+++ gatsby/src/components/profile/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import profileImg from "./hubert-lin.png";
+import "./profile.css";
 
 const Profile = () => (
-  <div>
-    <img className="" src={profileImg} alt="Photograph of Hubert Lin" />
-    <h1 className="">Hubert Lin</h1>
+  <div className="text-center">
+    <img src={profileImg} alt="Photograph of Hubert Lin" />
+    <h1>Hubert Lin</h1>
   </div>
 );
 
