27c83db6ed6b314af990b4a5a035bb7e6c3e7f09
Hubert Lin <linhub15@gmail.com>
2020-01-28 21:12:46 -0700

    center profile img

--- gatsby/src/components/profile/profile.js
+++ gatsby/src/components/profile/profile.js
@@ -4,7 +4,7 @@ import profileImg from "./hubert-lin.png";
 
 const Profile = () => (
   <div className="max-width-sm mx-auto p-1">
-    <img src={profileImg} alt="Photograph of Hubert Lin" />
+    <img className="d-block mx-auto" src={profileImg} alt="Photograph of Hubert Lin" />
     <h1>Hubert Lin</h1>
   </div>
 );
