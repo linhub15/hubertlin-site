f25ad373f6d32c2ea201a6a67dc2318db93f1608
Hubert Lin <linhub15@gmail.com>
2020-01-30 19:52:59 -0700

    add first blog post

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -5,10 +5,6 @@ module.exports = {
     author: `Hubert Lin`
   },
   plugins: [
-    `gatsby-plugin-catch-links`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -17,20 +13,17 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
     },
-    {
-      resolve: `gatsby-transformer-remark`
-    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
---
+++ gatsby/src/posts/post.md
@@ -0,0 +1,9 @@
+---
+path: "/blog/post"
+date: "2019-05-04"
+title: "My first blog post"
+---
+
+# some data
+
+hello this is my psot
