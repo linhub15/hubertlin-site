bdb3e1d723396951533505242b3d03f1e8384a7c
Hubert Lin <linhub15@gmail.com>
2020-02-09 14:51:15 -0700

    add link to blog

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import Profile from "../components/profile/profile";
 import Github from "../components/social-media/github";
@@ -12,6 +13,7 @@ const IndexPage = () => (
     <div className="social-media flex-col max-width-sm mx-auto p-1">
       <Github></Github>
       <Linkedin></Linkedin>
+      <Link to="/blog">Blog</Link>
     </div>
   </>
 );
